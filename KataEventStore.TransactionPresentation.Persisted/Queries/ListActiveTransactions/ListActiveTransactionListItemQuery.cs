f51edf4f001411518a09db5044cfb5e561c17a13
Pierre GILLON <contact@pierregillon.com>
2020-12-13 17:59:55 +0100

    Remove uncessary code

--- KataEventStore.TransactionPresentation.Persisted/Queries/ListActiveTransactions/ListActiveTransactionListItemQuery.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Collections.Generic;
-using MediatR;
-
-namespace KataEventStore.TransactionPresentation.Persisted.Queries.ListActiveTransactions {
-    public class ListActiveTransactionListItemQuery : IRequest<IEnumerable<TransactionListItem>> { }
-}
